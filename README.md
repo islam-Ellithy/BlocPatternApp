e3aa71cf9af95b200f19a15228efc5807d700e8b
Islam Emam <islam.emmo@gmail.com>
2020-03-19 20:47:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# bloc_app
+
+Movie app based on Bloc Pattern in flutter 
+
---
+++ lib/src/main.dart
@@ -0,0 +1,6 @@
+import 'package:bloc_app/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
